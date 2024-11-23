f267858eee9bdba147bf5147ed5914ef038962a9
rayaadinda <rayaadinda78@gmail.com>
2024-11-23 22:15:45 +0700

    feat(settings): implement settings tab functionality

--- app/src/main/java/com/unchain/ui/settings/viewpager/VPAdapter.kt
+++ app/src/main/java/com/unchain/ui/settings/viewpager/VPAdapter.kt
@@ -1,20 +1,23 @@
 package com.unchain.ui.settings.viewpager
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.unchain.ui.settings.viewpager.achievement.VpAchievementFragment
 import com.unchain.ui.settings.viewpager.settings.VpSettingsFragment
 import com.unchain.ui.settings.viewpager.userprofile.VpUserProfileFragment
 
-class VPAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 4
+class VPAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> VpUserProfileFragment()
             1 -> VpAchievementFragment()
             2 -> VpSettingsFragment()
-            else -> throw IllegalStateException("Invalid position: $position")
+            else -> throw IllegalArgumentException("Invalid position $position")
         }
     }
 }
